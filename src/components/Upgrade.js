8a2daa916a79386601328390fe0dba6f364678de
Brian Rosen <bwrosen20@gmail.com>
2024-09-02 11:09:06 -0400

    Style for upgrade

--- src/components/Upgrade.js
+++ src/components/Upgrade.js
@@ -7,12 +7,13 @@ function Upgrade({upgrade,getUpgrade}){
     return <div onClick={getUpgrade} value={upgrade["index"]}>
         <div className="upgrade" onMouseEnter={(()=>{setBlurb(true)})} onMouseLeave={(()=>{setBlurb(false)})}>
             <img src={upgrade["picture"]} value={upgrade["index"]} className="upgradePicture" alt="storePicture"/>
-            
         </div>
-        <div  className={blurb?"upgradeBlurb":"noBlurb"}>
-        <img src={upgrade["picture"]} className="upgradeBlurbPicture" alt="storePicture"/>
-            <h6>{upgrade["name"]}</h6>
-            <h6>😊{upgrade["price"]}</h6>
+        <div className={blurb?"upgradeBlurb":"noBlurb"}>
+            <div className="upgradeFirstRow">
+            <img src={upgrade["picture"]} className="upgradeBlurbPicture" alt="storePicture"/>
+                <h6 className="upgradeName">{upgrade["name"]}</h6>
+                <h6>😊{upgrade["price"]}</h6>
+            </div>
             <p className="upgradeDescription">{upgrade["description"]}</p>
         </div>
     </div>
