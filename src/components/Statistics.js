f2aa48c7b9201864be01fb05db111a63299ac5a3
Brian Rosen <bwrosen20@gmail.com>
2024-11-28 23:04:26 -0500

    Add style to upgrade list

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -2,6 +2,13 @@ import UpgradeItem from './UpgradeItem'
 
 function Statistics({upgrades}){
 
+    let amount = 0
+    upgrades.forEach((up)=>{
+        if (up["visible"]===3){
+            amount = amount+1
+        }
+    })
+
     return<div className="settingsScreen">
         <h1 className="heading">STATS</h1>
         <h4>Smiles In Bank: </h4>
@@ -10,6 +17,7 @@ function Statistics({upgrades}){
         
        
         <h3>Upgrades <span className="parenth">(scroll down)</span>:</h3>
+        <h6>Upgrades unlocked: {amount}/{upgrades.length}</h6>
         <div className="upgradeGrid">
             {upgrades.map((grade)=>(<UpgradeItem grade={grade}/>))}
         </div>
