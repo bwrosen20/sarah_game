accf4329696f11ac756614efdea1e237680a9e0f
Brian Rosen <bwrosen20@gmail.com>
2024-11-30 09:39:21 -0500

    Finish stats

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -1,19 +1,39 @@
 import UpgradeItem from './UpgradeItem'
+import numberify from '../numberify.js'
+import {useContext} from 'react'
+import {StoreItemsContext} from '../App'
 
-function Statistics({upgrades}){
+function Statistics({upgrades,clicksSoFar,soFar,clicks}){
+
+    const mainStoreItems = useContext(StoreItemsContext)
 
     let amount = 0
+    let smileMakers = 0
     upgrades.forEach((up)=>{
         if (up["visible"]===3){
             amount = amount+1
         }
     })
 
+    mainStoreItems.forEach((item)=>{
+        smileMakers = smileMakers + item["amount"]
+    })
+
+    let totalSoFar = 0
+
+    soFar.forEach((thing)=>{
+        totalSoFar = totalSoFar + thing["soFar"]
+    })
+
+    totalSoFar = numberify(totalSoFar + clicksSoFar)
+    const clickStringValue = numberify(clicks)
+    const smileMakerValue = numberify(smileMakers)
+
     return<div className="settingsScreen">
         <h1 className="heading">STATS</h1>
-        <h4>Smiles In Bank: </h4>
-        <h4>Smiles Made: </h4>
-        <h5>Smile Makers Owned: </h5>
+        <h4 className="lilStatsHeading" >Smiles In Bank: {clickStringValue}</h4>
+        <h4 className="lilStatsHeading">Smiles Made: {totalSoFar}</h4>
+        <h5 className="lilStatsHeading">Smile Makers Owned: {smileMakerValue}</h5>
         
        
         <h3 className="bigWords">Upgrades: <span className="parenth">(scroll down)</span></h3>
